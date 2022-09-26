0c9d0d2ccda07830994df84e85af3186854ec35b
Michael D. Hilborn <mhilborn@biggles2k.com>
2022-09-26 08:39:32 -0400

    Removed call to 'fetch' because it's not supported in Netlify node.js. Replaced with axios library.

--- simple-data-api/src/_data/doggy.js
+++ simple-data-api/src/_data/doggy.js
@@ -1,20 +1,14 @@
+const axios = require('axios').default; 
+
 module.exports = async function() {
  
-    // Fetch a random dog image URL from the Dog API
     try {
+        const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+        return response.data;
+      
+      } catch (error) {
+        console.error(error);
 
-        const dogData = 
-            await fetch("https://dog.ceo/api/breeds/image/random")
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
+      }
 
-        return dogData;
-
-    } catch (error) {
-        console.log(error);
-    }
 };
--- simple-data-api/src/_data/starwars.js
+++ simple-data-api/src/_data/starwars.js
@@ -1,22 +1,16 @@
 require('dotenv').config();
+const axios = require('axios').default; 
 
 module.exports = async function() {
  
-    // Fetch the top 10 headlines about "Star Wars" from newsapi.org
+    // Fetch the top 10 most recent articles about "Star Wars" from newsapi.org
     try {
-
         const starwarsHeadlines = 
-            await fetch("https://newsapi.org/v2/everything?q=%22star%20wars%22&pageSize=10&sortBy=publishedAt&language=en&apiKey=" + process.env.NEWS_API_KEY)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
-
-        return starwarsHeadlines;
+            await axios.get("https://newsapi.org/v2/everything?q=%22star%20wars%22&pageSize=10&sortBy=publishedAt&language=en&apiKey=" + process.env.NEWS_API_KEY);
+        return starwarsHeadlines.data;
 
     } catch (error) {
         console.error(error);
+
     }
 };
