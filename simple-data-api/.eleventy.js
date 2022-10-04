271cc0627f4bc9719921c7b482188ed0aaab8164
Michael D. Hilborn <mhilborn@biggles2k.com>
2022-10-04 18:40:04 -0400

    Geolocation API is now working; SSL configured.

--- simple-data-api/.eleventy.js
+++ simple-data-api/.eleventy.js
@@ -2,6 +2,12 @@ module.exports = function(config) {
  
     config.addPassthroughCopy("src/js");
     config.addPassthroughCopy("src/css");
+    config.setBrowserSyncConfig({
+        https: {
+            key: "./etc/localhost.key",
+            cert: "./etc/localhost.crt"
+        }
+    });
 
     return {
         dir: {
