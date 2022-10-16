b696281e5f8eadd397bcafdaf893e3c4c26fdc7a
Michael D. Hilborn <mhilborn@biggles2k.com>
2022-10-16 10:40:25 -0400

    Added client-side JS and a serverless function!

--- simple-data-api/src/js/weather.js
+++ simple-data-api/src/js/weather.js
@@ -4,20 +4,38 @@ const options = {
     maximumAge: 0
   };
   
-  function success(pos) {
-    const crd = pos.coords;
-  
+function success(pos) {
+    const url = `/.netlify/functions/weatherapi?lat=${crd.latitude}&lon=${crd.longitude}&units=metric`
+
     console.log('Your current position is:');
     console.log(`Latitude : ${crd.latitude}`);
     console.log(`Longitude: ${crd.longitude}`);
     console.log(`More or less ${crd.accuracy} meters.`);
-  }
-  
-  function error(err) {
+
+    fetch(url)
+    .then( (response) => {
+        return response.json();
+
+    })
+    .then( (data) => {
+        document.querySelector('#city').textContent = data.name;
+        document.querySelector('#temp').textContent = data.main.temp;
+        document.querySelector('#main').textContent = data.weather[0].main;
+        document.querySelector('#desc').textContent = data.weather[0].description;
+
+        console.log('data', data);
+
+    });
+
+}
+
+function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
+}
   
 
-document.addEventListener("DOMContentLoaded", function() { 
-    navigator.geolocation.getCurrentPosition(success, error, options);
+document.addEventListener("DOMContentLoaded", function() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    }
 });
